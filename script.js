cedf7d15c6b3b548947124c569f7fa9fe0c703a9
Rodrigo Falcão <125101340+rodrigo-falcao@users.noreply.github.com>
2023-04-11 20:16:43 -0300

    atualizações de alguns bugs e correção da contagem

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let gameOver = false;
 let loop;
 let counter = 0;
+let lastPipePosition = null;
 
 const mario = document.querySelector(".mario");
 const pipe = document.querySelector(".pipe");
@@ -12,8 +13,6 @@ const jump = () => {
         return;
     }
     mario.classList.add("jump");
-    counter++;
-    counterElement.innerHTML = counter;
 
     setTimeout(() => {
         mario.classList.remove("jump");
@@ -27,19 +26,24 @@ if (window.innerWidth <= 767) {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", " ");
 
-    if (pipePosition <= 80 && marioPosition < 10) {
-      pipe.style.animation = "none";
-      pipe.style.left = `${pipePosition}px`;
+    if (pipePosition <= 10) {
+      counter++;
+      counterElement.innerHTML = counter;
+      lastPipePosition = pipePosition;
+    } 
+        if (pipePosition <= 80 && marioPosition < 10) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
 
-      mario.style.animation = "none";
-      mario.style.bottom = `${marioPosition}px`;
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`;
 
-      mario.src = "./images/game-over.png";
-      mario.style.width = "35px";
-      mario.style.marginLeft = "50px";
+        mario.src = "./images/game-over.png";
+        mario.style.width = "35px";
+        mario.style.marginLeft = "50px";
 
-      clearInterval(loop);
-      gameOver = true;
+        clearInterval(loop);
+        gameOver = true;
     }
   }, 5); 
   document.addEventListener("touchstart", jump);
@@ -51,6 +55,12 @@ else {
   loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", " ");
+    
+    if (pipePosition <= 10) {
+      counter++;
+      counterElement.innerHTML = counter;
+      lastPipePosition = pipePosition;
+    }
 
     if (pipePosition <= 120 && marioPosition < 40) {
       pipe.style.animation = "none";
